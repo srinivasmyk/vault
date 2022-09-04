dae2bdaa17d55e040fb14892be8a6be52e6b3533
srinivas <srinivas.myakala18@gmail.com>
2022-09-04 23:05:24 +0530

    log4

--- script.js
+++ script.js
@@ -40,6 +40,28 @@ password:14
 //////////////////////////
 
 //////////user validation///////////
+
+///temp PX identify
+
+aptrinsic("identify",
+{
+//User Fields
+  "id": "unique-user-id", // Required for logged in app users
+  "email": "userEmail@address.com",
+  "firstName": "John",
+  "lastName": "Smith",
+  "signUpDate": 1522697426479, //unix time in ms
+  "plan" : "gold", //Custom attributes - please create those custom attributes in Aptrinsic via Account Settings to be tracked.
+  "price" : 95.5,
+  "userHash": "" // optional transient for HMAC identification
+},
+{
+//Account Fields
+  "id":"IBM", //Required
+  "name":"International Business Machine",
+  "Program": "Platinum" // flat custom attributes
+});
+////////
 function validateUser(){
 
   if(userEmail=="srini3@gmail.com" && userPassword=="14"){
