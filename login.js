03dc6578fe36e675e789b6a5e92de635475df263
srinivas <srinivas.myakala18@gmail.com>
2022-09-05 14:45:47 +0530

    new login JS

---
+++ login.js
@@ -0,0 +1,47 @@
+let [user] = [{
+    email: "srini1@gmail.com",
+    password:12
+  }, 
+  {
+    email: "srini2@gmail.com",
+  password:13
+  },
+  {
+    email: "srini3@gmail.com",
+  password:14
+  }];
+  
+  //////////////////////////
+  
+  //////////user validation///////////
+  
+  ///temp PX identify
+  
+  
+  ////////
+  function validateUser(){
+  console.log(userEmail,userPassword);
+    if(userEmail=="srini@gmail.com"){
+      //passing user and account objects:
+  aptrinsic("identify",
+  {
+  //User Fields
+    "id": "unique-user-id", // Required for logged in app users
+    "email": "userEmail@address.com",
+    "firstName": "John",
+    "lastName": "Smith",
+    "signUpDate": 1522697426479, //unix time in ms
+    "plan" : "gold", //Custom attributes - please create those custom attributes in Aptrinsic via Account Settings to be tracked.
+    "price" : 95.5,
+    "userHash": "" // optional transient for HMAC identification
+  },
+  {
+  //Account Fields
+    "id":"IBM", //Required
+    "name":"International Business Machine",
+    "Program": "Platinum" // flat custom attributes
+  });
+  return true;
+    }
+  return false;
+  };
--- script.js
+++ script.js
@@ -22,55 +22,9 @@ const userEmail=document.querySelector('#userEmail').value;
 const userPassword=document.querySelector('#userPassword').value;
 
 
-///////////Dummy user data///////dfgdfg
-
-let [user] = [{
-  email: "srini1@gmail.com",
-  password:12
-}, 
-{
-  email: "srini2@gmail.com",
-password:13
-},
-{
-  email: "srini3@gmail.com",
-password:14
-}];
-
-//////////////////////////
-
-//////////user validation///////////
-
-///temp PX identify
 
 
-////////
-function validateUser(){
-console.log(userEmail,userPassword);
-  if(userEmail=="srini@gmail.com"){
-    //passing user and account objects:
-aptrinsic("identify",
-{
-//User Fields
-  "id": "unique-user-id", // Required for logged in app users
-  "email": "userEmail@address.com",
-  "firstName": "John",
-  "lastName": "Smith",
-  "signUpDate": 1522697426479, //unix time in ms
-  "plan" : "gold", //Custom attributes - please create those custom attributes in Aptrinsic via Account Settings to be tracked.
-  "price" : 95.5,
-  "userHash": "" // optional transient for HMAC identification
-},
-{
-//Account Fields
-  "id":"IBM", //Required
-  "name":"International Business Machine",
-  "Program": "Platinum" // flat custom attributes
-});
-return true;
-  }
-return false;
-};
+
 
 //////////////////////////////
 
@@ -142,15 +96,15 @@ document.querySelector(".btn--close-cookie").addEventListener('click',function()
 ////scrolll
 
 
-// btnScrollTo.addEventListener('click', (e) =>{
-//   const s1Cords = section1.getBoundingClientRect();
-// // window.scrollTo({
-// //   left: s1Cords.left + window.pageXOffset,
-// //   top: s1Cords.top + window.pageYOffset,
-// //   behavior: 'smooth',
-// // });
-// section1.scrollIntoView({behavior:'smooth'});
+btnScrollTo.addEventListener('click', (e) =>{
+  const s1Cords = section1.getBoundingClientRect();
+// window.scrollTo({
+//   left: s1Cords.left + window.pageXOffset,
+//   top: s1Cords.top + window.pageYOffset,
+//   behavior: 'smooth',
 // });
+section1.scrollIntoView({behavior:'smooth'});
+});
 
 ///////page navigation///////
 
