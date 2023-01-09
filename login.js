53f9977e3c91fdd85cf50babe795ddf4687e1de8
srinivasmyk <65235119+srinivasmyk@users.noreply.github.com>
2023-01-09 21:16:29 +0530

    Update login.js

--- login.js
+++ login.js
@@ -25,26 +25,28 @@ var userPassword=document.getElementById('userPassword').value;
   console.log("email"+ userEmail+ "pwd :"+userPassword);
     if(userEmail=="srini@gmail.com"){
       //passing user and account objects:
-       aptrinsic("identify",
-  {
-  //User Fields
-    "id": "unique-user-id", // Required for logged in app users
-    "email": "userEmail@address.com",
-    "firstName": "John",
-    "lastName": "Smith",
-    "signUpDate": 1522697426479, //unix time in ms
-    "plan" : "gold", //Custom attributes - please create those custom attributes in Aptrinsic via Account Settings to be tracked.
-    "price" : 95.5,
-    "userHash": "" // optional transient for HMAC identification
-  },
-  {
-  //Account Fields
-    "id":"IBM", //Required
-    "name":"International Business Machine",
-    "Program": "Platinum" // flat custom attributes
-  });
+//        aptrinsic("identify",
+//   {
+//   //User Fields
+//     "id": "unique-user-id", // Required for logged in app users
+//     "email": "userEmail@address.com",
+//     "firstName": "John",
+//     "lastName": "Smith",
+//     "signUpDate": 1522697426479, //unix time in ms
+//     "plan" : "gold", //Custom attributes - please create those custom attributes in Aptrinsic via Account Settings to be tracked.
+//     "price" : 95.5,
+//     "userHash": "" // optional transient for HMAC identification
+//   },
+//   {
+//   //Account Fields
+//     "id":"IBM", //Required
+//     "name":"International Business Machine",
+//     "Program": "Platinum" // flat custom attributes
+//   });
+        aptrinsic('identify', {"id": "ail.c"})
+
   
- // return true;
+ return true;
     }
  
   };
