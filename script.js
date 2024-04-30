34ff2aea12ec4122a1c4114188c43e709ea6b7bf
srinivasmyk <65235119+srinivasmyk@users.noreply.github.com>
2024-05-01 02:00:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -428,4 +428,23 @@ function PXPageTimer(maxSecondsTracked, trackPagesOverMax) {
   };
 })();
 
+//////// modal logic
+var modal = document.getElementById('myModal');
+
+// Function to open the modal
+function openModal() {
+  modal.style.display = 'block';
+}
+
+// Function to close the modal
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+// Close the modal when the user clicks anywhere outside of it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+}
 
